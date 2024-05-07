8366ff3452e715b342f505e516e4cc92d5fc96cc
Lance Munyao <munyaolance1@gmail.com>
2024-05-07 15:47:51 +0300

    Feat: Models and Migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('role', ['user', 'sacco_admin', 'driver', 'admin'])->default('user');
+            $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->time('last_login')->nullable();
+            $table->time('last_logout')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
---
+++ database/migrations/2024_05_07_123829_create_vehicles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->string('number_plate')->unique();
+            $table->enum('type', ['bus', 'matatu', 'taxi', 'lorry', 'motorcycle', 'bicycle'])->default('bus');
+            $table->string('color');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
