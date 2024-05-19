48423e5c1bd18d1ca1fa12554dabb8da6e69efec
Lance Munyao <munyaolance1@gmail.com>
2024-05-19 06:20:39 +0300

    :coffin: Fix: Removed unnecessary commented code

--- routes/web.php
+++ routes/web.php
@@ -39,17 +39,15 @@ Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
 Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
 
 
-// Route::middleware(['auth'])->group(function () {
-    // SACCO routes
-    Route::get('sacco/home', SaccoHome::class)->name('sacco_admin.home')->middleware('auth:sacco_admin')->middleware(saccomid::class);
+// SACCO routes
+Route::get('sacco/home', SaccoHome::class)->name('sacco_admin.home')->middleware('auth:sacco_admin')->middleware(saccomid::class);
 
-    // Admin routes
-    Route::get('admin/home', AdminHome::class)->name('admin.home')->middleware('auth:admin')->middleware(adminmid::class);
+// Admin routes
+Route::get('admin/home', AdminHome::class)->name('admin.home')->middleware('auth:admin')->middleware(adminmid::class);
 
-    // User routes
-    Route::get('users/home', UsersHome::class)->name('user.home')->middleware('auth:users')->middleware(usermid::class);
+// User routes
+Route::get('users/home', UsersHome::class)->name('user.home')->middleware('auth:users')->middleware(usermid::class);
 
-    // Driver routes
-    Route::get('driver/home', DriverHome::class)->name('driver.home')->middleware('auth:driver')->middleware(drivermid::class);
+// Driver routes
+Route::get('driver/home', DriverHome::class)->name('driver.home')->middleware('auth:driver')->middleware(drivermid::class);
 
-// });
