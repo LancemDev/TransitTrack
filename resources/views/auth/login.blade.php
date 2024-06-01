c57b7a83149eadfb39fae22b02b7658483b66471
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-06-01 15:04:49 +0300

    Changed the theme. Completed theme setting and css on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -113,14 +113,14 @@
               <!-- Your form elements go here -->
               <div>
                 <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-                <input type="text" id="email" name="email" class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300">
+                <input type="text" id="email" name="email" style="color: white;" class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-300 transition-colors duration-300">
               </div>
               <div>
                 <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-                <input type="password" id="password" name="password" class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300">
+                <input type="password" id="password" name="password" style="color: white;" class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-300 transition-colors duration-300">
               </div>
               <div>
-                <input type="submit" class="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300" value="Log In"/>
+                <input type="submit" style="cursor: pointer" class="w-full bg-black text-white p-2 rounded-md hover:action-call focus:outline-none focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300" value="Log In"/>
               </div>
             </form>
           </div>
