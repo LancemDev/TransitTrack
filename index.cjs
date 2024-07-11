8b7480fa2ed86dcd039ad7d81e24864d41bfd8ce
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-07-11 14:18:25 +0300

    New packages installed

--- index.cjs
+++ index.cjs
@@ -1,15 +1,42 @@
 const jsonfile = require('jsonfile');
-const moment = require('moment')
-const simpleGit = require('simple-git')
-const FIlE_PATH = './data.json';
+const moment = require('moment');
+const mathRandom = require('math-random');
+const simpleGit = require('simple-git')();
+const FILE_PATH = './data.json';
 
-const DATE = moment().format();
+const randomInt = (min, max) => {
+    return Math.floor(mathRandom() * (max - min + 1)) + min;
+};
 
-const data = {
-    date: DATE
-}
+const makeCommit = async (n) => {
+    if (n === 0) {
+        try {
+            await simpleGit.push();
+        } catch (err) {
+            console.error('Error pushing to git:', err);
+        }
+        return;
+    }
 
-jsonfile.writeFile(FIlE_PATH, data)
+    const x = randomInt(0, 6);
+    const y = randomInt(0, 7);
 
-// git commit --date = "certain date"
-simpleGit().add([FIlE_PATH]).commit(DATE, {'--date' : DATE}).push();
+    const DATE = moment().subtract(6, 'w').add(x, 'w').add(y, 'd').format();
+    const data = { date: DATE };
+    console.log(DATE);
+
+    jsonfile.writeFile(FILE_PATH, data, (err) => {
+        if (err) {
+            console.error('Error writing to file:', err);
+            return;
+        }
+        simpleGit.add([FILE_PATH])
+            .then(() => simpleGit.commit('Commit new changes', { '--date': DATE }))
+            .then(() => makeCommit(n - 1))
+            .catch((err) => {
+                console.error('Error during git operation:', err);
+            });
+    });
+};
+
+makeCommit(50);
