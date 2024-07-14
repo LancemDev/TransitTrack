1fbb228be0c66d6c495b42de7b6c8e0acfc91456
LancemDev <munyaolance1@gmail.com>
2024-07-15 02:29:45 +0300

    Feat: Add Admin Seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Admin::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'phone' => '1234567890',
+        ]);
+    }
+}
