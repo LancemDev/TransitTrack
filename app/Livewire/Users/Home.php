0e42a5850416878fd50735636ca9ae31a4da272a
LancemDev <munyaolance1@gmail.com>
2024-07-15 04:57:55 +0300

    :ambulance: Feat: Used relevant seeder values

--- app/Livewire/Users/Home.php
+++ app/Livewire/Users/Home.php
@@ -5,9 +5,26 @@ namespace App\Livewire\Users;
 
 use Livewire\Component;
 use Mary\Traits\Toast;
+use App\Models\User;
+use App\Models\Vehicle;
+use App\Models\Route;
+use App\Models\Sacco;
 
 class Home extends Component
 {
+    use Toast;
+
+    public bool $showModal = false;
+
+    public $busDetails = [];
+
+    protected $listeners = ['openModal'];
+
+    public function openModal($busDetails)
+    {
+        $this->busDetails = $busDetails;
+        $this->showModal = true;
+    }
     public function render()
     {
         return view('livewire.users.home');
